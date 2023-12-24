152d4d3296591b536831093f2cafff904c45db67
Paulo Rubens <paulu_rubens@hotmail.com>
2023-12-24 10:08:33 -0300

    Added small visor and logic display changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Variables Declaration
 const visor = document.querySelector('#visor');
+const small_visor = document.querySelector('#small_visor');
 const calculator = document.querySelector('#main_div');
 const btns_both = document.querySelectorAll('.number, .operator');
 const btns_number = document.querySelectorAll('.number');
@@ -11,6 +12,8 @@ let second_number = null;
 let symbol = null;
 let prev_first_number = '';
 let prev_second_number = '';
+let contador = 0;
+let small_visor_control = false;
 // ----------------------------------------------------------------------------------------
 // Functions
 
@@ -42,7 +45,7 @@ calculator.addEventListener('click', function(event) {
             if(first_number === null && symbol === null && second_number === null){
                 prev_first_number += target.textContent;
             }
-            if(first_number != null && symbol != null && second_number === null){
+            else if(first_number != null && symbol != null && second_number === null){
                 prev_second_number += target.textContent;
             }
         }
@@ -51,58 +54,65 @@ calculator.addEventListener('click', function(event) {
 
             if(first_number === null && symbol === null && second_number === null){
                 first_number = parseInt(prev_first_number);
-                visor.textContent = visor.textContent + ' ' + target.textContent + ' ';
+                small_visor.textContent = prev_first_number + ' ' + target.textContent + ' ';
+                visor.textContent = '';
                 symbol = target.textContent;
-                btns_operator.forEach(function(btn) {
-                     btn.disabled = true});
+            }
+            else if(symbol != null){
+                small_visor.textContent = prev_first_number + ' ' + target.textContent + ' ';
+                symbol = target.textContent;
             }
         }
 
         if(target.classList.contains('equal')){
             if(first_number != null && symbol != null && second_number != null || prev_second_number != ''){
                 second_number = parseInt(prev_second_number);
+                if(small_visor_control === false)
+                    small_visor.textContent = small_visor.textContent + prev_second_number;
+                    small_visor_control = true;
                 if(symbol === '+'){
                     result = addNumber(first_number, second_number);
                     visor.textContent = result;
-                    btns_number.forEach(function(btn) {
+                    btns_both.forEach(function(btn) {
                     btn.disabled = true});
                 }
                 else if(symbol === '-'){
                     result = subNumber(first_number, second_number);
                     visor.textContent = result;
-                    btns_number.forEach(function(btn) {
+                    btns_both.forEach(function(btn) {
                     btn.disabled = true});
                 }
                 else if(symbol === '*'){
                     result = multNumber(first_number, second_number);
                     visor.textContent = result;
-                    btns_number.forEach(function(btn) {
+                    btns_both.forEach(function(btn) {
                         btn.disabled = true});
                 }
                 else if(symbol === '/' && second_number === 0){
                     visor.textContent = "Não é possível dividir por 0.";
-                    btns_number.forEach(function(btn) {
+                    btns_both.forEach(function(btn) {
                     btn.disabled = true});
                 }
                 else if(symbol === '/'){
                     result = divNumber(first_number, second_number);
                     visor.textContent = result;
-                    btns_number.forEach(function(btn) {
+                    btns_both.forEach(function(btn) {
                     btn.disabled = true});
                 }
-
-                console.log(second_number, symbol)
             }
         }
         
 
         if(target.textContent === 'Limpar'){
             visor.textContent = '';
+            small_visor.textContent = '';
             first_number = null;
             second_number = null;
             symbol = null;
             prev_first_number = '';
             prev_second_number = '';
+            contador = 0;
+            small_visor_control = false;
 
             btns_both.forEach(function(btn) {
                 btn.disabled = false});
